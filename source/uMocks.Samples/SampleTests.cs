2514ee957904a4795fdf945ec931531b53e92b35
Przemysław Smokowski <przemyslaw.smokowski@gmail.com>
2019-11-27 19:11:37 +0100

    Added sample tests project

---
+++ source/uMocks.Samples/SampleTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace uMocks.Samples
+{
+  [TestClass]
+  public class SampleTests
+  {
+    [TestMethod]
+    public void TestMethod()
+    {
+    }
+  }
+}
